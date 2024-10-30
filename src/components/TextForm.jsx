c50912eae774ff5c06203e4ef1701a26bc462e5f
Ketan Sharma <ketan10923@gmail.com>
2024-10-31 00:43:43 +0100

    More Button-functions Added

--- src/components/TextForm.jsx
+++ src/components/TextForm.jsx
@@ -12,9 +12,62 @@ export default function TextForm(props) {
     const newText = text.toLowerCase();
     setText(newText);
   };
-  const HandleOnChange = (event) => {
+  const handleOnChange = (event) => {
     console.log("On Changge Clicked");
     setText(event.target.value);
+  };
+  const handleClearClick = () => {
+    console.log("Clear text clicked");
+    const newText = "";
+    setText(newText);
+  };
+  const handleCamelCaseClick = (str) => {
+    console.log("CamelCase Clicked");
+    str = text;
+    const words = str
+      .toLowerCase()
+      .replace(/[^\w\s]/g, "")
+      .split(" ");
+    const camelCase = words
+      .map((word, index) => {
+        if (index == 0) {
+          return word;
+        } else {
+          return word.charAt(0).toUpperCase() + word.slice(1);
+        }
+      })
+      .join("");
+    setText(camelCase);
+  };
+  const handlePascalCaseClick = () => {
+    console.log("PascalCase Clicked");
+    const words = text
+      .trim()
+      .toLowerCase()
+      .replace(/[^\w\s]/g, "")
+      .split(" ");
+    const pascalCase = words
+      .map((word) => {
+        return word.charAt(0).toUpperCase() + word.slice(1);
+      })
+      .join("");
+    setText(pascalCase);
+  };
+  const handleInverseCaseClick = () => {
+    console.log("InverseCase Clicked");
+    const words = text.replace(/[^\w\s]/g, "").split(" ");
+    const inverseCase = words.map((word) => {
+      const charArray = word.split("");
+   const invertedChar=  charArray.map((element) => {
+        if (element == element.toUpperCase()) {
+          return element.toLowerCase();
+        } else if (element == element.toLowerCase()) {  
+          return element.toUpperCase();
+        }
+      });
+      return invertedChar.join('');
+    }).join(' ');
+    setText(inverseCase); 
   };
 
   return (
@@ -29,7 +82,7 @@ export default function TextForm(props) {
             id="myBox"
             className="border-2 p-5"
             rows={5}
-            onChange={HandleOnChange}
+            onChange={handleOnChange}
             value={text}
           ></textarea>
 
@@ -37,16 +90,38 @@ export default function TextForm(props) {
             <button
               className="border border-black text-white bg-blue-400 w-fit p-2 rounded-2xl "
               onClick={handleUpClick}
-              id="UpButton"
             >
-              Change to UpperCase
+              Change to UPPERCASE
             </button>
             <button
               className="border border-black text-white bg-blue-400 w-fit p-2 rounded-2xl "
               onClick={handleLoClick}
-              id="UpButton"
             >
-              Change to LowerCase
+              Change to lowercase
+            </button>
+            <button
+              className="border border-black text-white bg-blue-400 w-fit p-2 rounded-2xl "
+              onClick={handleClearClick}
+            >
+              Clear Text
+            </button>
+            <button
+              className="border border-black text-white bg-blue-400 w-fit p-2 rounded-2xl "
+              onClick={handleCamelCaseClick}
+            >
+              Change to camelCase
+            </button>
+            <button
+              className="border border-black text-white bg-blue-400 w-fit p-2 rounded-2xl "
+              onClick={handlePascalCaseClick}
+            >
+              Change to PascalCase
+            </button>
+            <button
+              className="border border-black text-white bg-blue-400 w-fit p-2 rounded-2xl "
+              onClick={handleInverseCaseClick}
+            >
+              Change to InverseCase
             </button>
           </div>
         </div>
@@ -57,6 +132,10 @@ export default function TextForm(props) {
           <p> Words: {text.split(" ").length} </p>
           <p> Characters : {text.length}</p>
         </div>
+      </div>
+      <div className="mt-5">
+        <p className="text-4xl">Preview</p>
+        <p className="overflow-auto h-36 border border-black p-5 rounded-xl mt-3">{text}</p>
       </div>
     </div>
   );
